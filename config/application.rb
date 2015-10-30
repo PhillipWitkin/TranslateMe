8bba1a552cb44fda6ab0b00735ac68929fee71ec
Phillip Witkin <pwitkin273@gmail.com>
2015-10-30 17:55:54 -0400

    changed location of initialize_on_precompile to within application class

--- config/application.rb
+++ config/application.rb
@@ -1,14 +1,14 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
-config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module TranslateMeApp
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
