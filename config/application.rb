ab8014e7a817129feb26bbe85903879c3c6ff15a
Phillip Witkin <pwitkin273@gmail.com>
2015-10-30 19:28:36 -0400

    tried anoter thing, in applicaiton.rb

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+config.assets.initialize_on_precompile = false
+config.assets.compile = true
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,7 +12,6 @@ Bundler.require(*Rails.groups)
 module TranslateMeApp
   class Application < Rails::Application
 
-    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
